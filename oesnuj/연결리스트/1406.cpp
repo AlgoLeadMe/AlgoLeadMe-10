18493c70780cf2e22b695ff6d30b02ec507c072d
Junseo Kim <semiwest2@gmail.com>
2024-03-30 00:04:02 +0900

    2024-03-29 에디터

---
+++ oesnuj/연결리스트/1406.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    list <char> li; //양방향 연결리스트 선언
+    string str;
+    cin >> str;
+    for (auto c : str) //한 문자씩 연결리스트에 넣기
+        li.push_back(c);
+
+    list<char>::iterator it = li.end();
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        char input;
+        cin >> input;
+        if (input == 'L')
+        {
+            if (it != li.begin())
+                it--;
+        }
+        else if (input == 'D')
+        {
+            if (it != li.end())
+                it++;
+        }
+        else if (input == 'B')
+        {
+            if (it != li.begin())
+            {
+                it--;
+                it = li.erase(it);
+            }   
+        }
+        else if (input == 'P')
+        {
+            char insertChar;
+            cin >> insertChar;
+            it = li.insert(it, insertChar);
+            it++;
+        }
+    }
+    
+    for (auto c : li)
+        cout << c;
+    return 0;
+}
